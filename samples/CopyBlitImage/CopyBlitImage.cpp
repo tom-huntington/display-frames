51d77a6375b39d296b592b7ecaac2fd595e8b691
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2023-10-07 09:33:30 +1300

    show image implemented

--- samples/CopyBlitImage/CopyBlitImage.cpp
+++ samples/CopyBlitImage/CopyBlitImage.cpp
@@ -18,12 +18,39 @@
 #include "../utils/utils.hpp"
 
 #include <thread>
+#include <string_view>
 
 static char const * AppName    = "CopyBlitImage";
 static char const * EngineName = "Vulkan.hpp";
 
-int main( int /*argc*/, char ** /*argv*/ )
+int main( int argc, char ** argv )
 {
+  const std::vector<std::string_view> args(argv + 1, argv + argc);
+  auto it = std::find(args.begin(), args.end(), std::string_view{"--size"});
+  if (it == args.end())
+  {
+    std::cout << "No --size {width}x{height} command line argument provided" << std::endl;
+    return 1;
+  }
+  std::advance(it, 1);
+  int width_input_ = 1280;//720][1280
+  int height_input_ = 720;
+  sscanf( it->data(), "%dx%d", &width_input_, &height_input_);
+  const unsigned width_input = width_input_;
+  const unsigned height_input = height_input_;
+  const int width_swapchain = 1280;
+  const int height_swapchain = 720;
+
+  auto input_data = std::vector<uint8_t>(3*width_input*height_input);
+  size_t read_size;
+  for (int i = 0; i < 1; i++)
+  {
+    read_size = fread(input_data.data(), 1, input_data.size(), stdin);
+    assert(input_data.size() == read_size);
+  }
+
+  std::printf("read\n");
+
   try
   {
     vk::Instance instance = vk::su::createInstance( AppName, EngineName, {}, vk::su::getInstanceExtensions() );
@@ -33,7 +60,7 @@ int main( int /*argc*/, char ** /*argv*/ )
 
     vk::PhysicalDevice physicalDevice = instance.enumeratePhysicalDevices().front();
 
-    vk::su::SurfaceData surfaceData( instance, AppName, vk::Extent2D( 640, 640 ) );
+    vk::su::SurfaceData surfaceData( instance, AppName, vk::Extent2D( width_swapchain, height_swapchain ) );
 
     vk::SurfaceCapabilitiesKHR surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR( surfaceData.surface );
     if ( !( surfaceCapabilities.supportedUsageFlags & vk::ImageUsageFlagBits::eTransferDst ) )
@@ -66,7 +93,7 @@ int main( int /*argc*/, char ** /*argv*/ )
 #if 1 // !defined( NDEBUG )
     //auto f = swapChainData.colorFormat;
     //assert(f);
-    vk::FormatProperties formatProperties = physicalDevice.getFormatProperties( vk::Format::eB8G8R8A8Unorm/*swapChainData.colorFormat*/ );
+    vk::FormatProperties formatProperties = physicalDevice.getFormatProperties( /*vk::Format::eB8G8R8A8Unorm*/swapChainData.colorFormat );
     //auto b = bool(formatProperties.linearTilingFeatures & vk::FormatFeatureFlagBits::eBlitDst);
     auto b2 = bool( formatProperties.optimalTilingFeatures & vk::FormatFeatureFlagBits::eBlitSrc );
     assert( b2 && "Format cannot be used as transfer source" );
@@ -89,7 +116,7 @@ int main( int /*argc*/, char ** /*argv*/ )
     vk::ImageCreateInfo imageCreateInfo( vk::ImageCreateFlags(),
                                          vk::ImageType::e2D,
                                          swapChainData.colorFormat,
-                                         vk::Extent3D( 32, 32, 1 ),
+                                         vk::Extent3D( width_input, height_input, 1 ),
                                          1,
                                          1,
                                          vk::SampleCountFlagBits::e1,
@@ -110,7 +137,7 @@ int main( int /*argc*/, char ** /*argv*/ )
     vk::ImageCreateInfo imageCreateInfo2( vk::ImageCreateFlags(),
                                          vk::ImageType::e2D,
                                          swapChainData.colorFormat,
-                                         vk::Extent3D( 32, 32, 1 ),
+                                         vk::Extent3D( width_input, height_input, 1 ),
                                          1,
                                          1,
                                          vk::SampleCountFlagBits::e1,
@@ -142,17 +169,28 @@ int main( int /*argc*/, char ** /*argv*/ )
     unsigned char * pImageMemory = static_cast<unsigned char *>( device.mapMemory( deviceMemory, 0, memoryRequirements.size ) );
     auto layout = device.getImageSubresourceLayout( blitSourceImage, vk::ImageSubresource( vk::ImageAspectFlagBits::eColor ) );
     
-    // Checkerboard of 8x8 pixel squares
-    for ( uint32_t row = 0; row < 32; row++ )
+    if (not (swapChainData.colorFormat == vk::Format::eB8G8R8A8Unorm || swapChainData.colorFormat == vk::Format::eR8G8B8A8Unorm))
     {
-      for ( uint32_t col = 0; col < 32; col++ )
+      std::cout << "Device has unsupported color format. Only B8G8R8A8Unorm and R8G8B8A8Unorm supported" << std::endl;
+      return 1;
+    }
+    auto color_index0 = vk::Format::eB8G8R8A8Unorm == swapChainData.colorFormat ? 0 : 2;
+    auto color_index2 = vk::Format::eB8G8R8A8Unorm == swapChainData.colorFormat ? 2 : 0;
+    // Checkerboard of 8x8 pixel squares
+    for ( uint32_t row = 0; row < height_input; row++ )
+    {
+      for ( uint32_t col = 0; col < width_input; col++ )
       {
         //unsigned char rgb = ( ( ( row & 0x4 ) == 0 ) ^ ( ( col & 0x4 ) == 0 ) ) * 255;
-        unsigned char rgb = (((row / 4) % 2) == ((col / 4) % 2 )) * 255;
-        auto ImageMemory = pImageMemory + ((layout.offset  + col)*4 + row * layout.rowPitch);
-        ImageMemory[0]   = rgb;
-        ImageMemory[1]   = rgb;
-        ImageMemory[2]   = rgb;
+        //unsigned char rgb = (((row / 4) % 2) == ((col / 4) % 2 )) * 255;
+        auto offset = ((layout.offset  + col)*4 + row * layout.rowPitch);
+        auto ImageMemory = pImageMemory + offset;
+        auto color = input_data[(row * width_input + col) * 3 + color_index0];
+        ImageMemory[0]   = color;
+        color = input_data[(row * width_input + col) * 3 + 1];
+        ImageMemory[1]   = color;
+        color = input_data[(row * width_input + col) * 3 + color_index2];
+        ImageMemory[2]   = color;
         ImageMemory[3]   = 255;
         //pImageMemory += 4;
       }
@@ -172,7 +210,7 @@ int main( int /*argc*/, char ** /*argv*/ )
     vk::su::setImageLayout( commandBuffer, blitSourceImage2, swapChainData.colorFormat, vk::ImageLayout::eGeneral, vk::ImageLayout::eTransferDstOptimal );
 
     vk::ImageSubresourceLayers imageSubresourceLayers( vk::ImageAspectFlagBits::eColor, 0, 0, 1 );
-    vk::ImageCopy imageCopy( imageSubresourceLayers, vk::Offset3D(), imageSubresourceLayers, vk::Offset3D( 0, 0, 0 ), vk::Extent3D( 32, 32, 1 ) );
+    vk::ImageCopy imageCopy( imageSubresourceLayers, vk::Offset3D(), imageSubresourceLayers, vk::Offset3D( 0, 0, 0 ), vk::Extent3D( width_input, height_input, 1 ) );
     commandBuffer.copyImage( blitSourceImage, vk::ImageLayout::eTransferSrcOptimal, blitSourceImage2, vk::ImageLayout::eTransferDstOptimal, imageCopy );
 
 
@@ -180,7 +218,7 @@ int main( int /*argc*/, char ** /*argv*/ )
 
     // Do a 32x32 blit to all of the dst image - should get big squares
     vk::ImageBlit              imageBlit( imageSubresourceLayers,
-                                          { { vk::Offset3D( 0, 0, 0 ), vk::Offset3D( 32, 32, 1 ) } },
+                                          { { vk::Offset3D( 0, 0, 0 ), vk::Offset3D( width_input, height_input, 1 ) } },
                              imageSubresourceLayers,
                                           { { vk::Offset3D( 0, 0, 0 ), vk::Offset3D( surfaceData.extent.width, surfaceData.extent.height, 1 ) } } );
     
@@ -202,8 +240,8 @@ int main( int /*argc*/, char ** /*argv*/ )
       vk::PipelineStageFlagBits::eTransfer, vk::PipelineStageFlagBits::eTransfer, vk::DependencyFlags(), nullptr, nullptr, memoryBarrier );
 
     // Do a image copy to part of the dst image - checks should stay small
-    vk::ImageCopy imageCopy2( imageSubresourceLayers, vk::Offset3D(), imageSubresourceLayers, vk::Offset3D( 256, 256, 0 ), vk::Extent3D( 32, 32, 1 ) );
-    commandBuffer.copyImage( blitSourceImage, vk::ImageLayout::eTransferSrcOptimal, blitDestinationImage, vk::ImageLayout::eTransferDstOptimal, imageCopy2 );
+    //vk::ImageCopy imageCopy2( imageSubresourceLayers, vk::Offset3D(), imageSubresourceLayers, vk::Offset3D( 256, 256, 0 ), vk::Extent3D( width_input, height_input, 1 ) );
+    //commandBuffer.copyImage( blitSourceImage, vk::ImageLayout::eTransferSrcOptimal, blitDestinationImage, vk::ImageLayout::eTransferDstOptimal, imageCopy2 );
 
     // Note: for a layout of vk::ImageLayout::ePresentSrcKHR, the access mask is supposed to be empty
     vk::ImageMemoryBarrier prePresentBarrier( vk::AccessFlagBits::eTransferWrite,
